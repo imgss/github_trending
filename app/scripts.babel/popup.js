1af54d0c2b82e99a858915b1eb401bc781992387
imgss <838632526@qq.com>
2018-11-21 10:54:08 +0800

    refine the code

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,38 +1,52 @@
 'use strict';
 let article = document.querySelector('article'),
 selector = document.getElementById('cache');
-//生成日期选项
-selector.innerHTML = Object
-                .keys(localStorage)
-                .map(key => `<option value="${key}">${key}</option>`)
-                .join('');
-
+// 根据localstorage生成日期选项
+let historyArr = Object.keys(localStorage);
+selector.innerHTML = historyArr.map(key => `<option value="${key}">${key}</option>`).join('');
 selector.onchange = function(){
     article.innerHTML = localStorage.getItem(selector.selectedOptions[0].value);
 }
+
 // 获取今天的trending
-let today = new Date().getDate()
+let now = new Date();
+let now_date = now.getDate();
+let today = (now.getMonth() + 1) + '-' + now_date;
 let html = localStorage.getItem(today);
 if(html){
     article.innerHTML = html;
-    selector.value = new Date().getDate() + '';
+    selector.value = today;
 }else{
+    // 拉取新的trending
     chrome.browserAction.setBadgeText({text: 'load'});
     chrome.browserAction.setBadgeBackgroundColor({color: [0, 0, 200, 255]});
+
     axios.get('https://github.com/trending').then(response => {
         let trendList = /<ol[\s\S]+\<\/ol>/m.exec(response.data);
         chrome.browserAction.setBadgeText({text: 'done'});
         chrome.browserAction.setBadgeBackgroundColor({color: [0, 0, 200, 255]});
         let html = trendList[0]
         html = html.replace(/href="\/(\S+)"/gm,'href="https://github.com/$1"')
-        localStorage.setItem(new Date().getDate(), html);
+        localStorage.setItem(today, html);
         article.innerHTML = html;
         let option = document.createElement('option');
-        option.textContent = new Date().getDate() + '';
+        option.textContent = today;
+        option.selected = true;
         selector.appendChild(option);
     }).catch(e => {
         alert('数据获取不成功');
     })
+
+    // 如果有上个月当天的缓存，则清除
+    let dayReg = new RegExp('-' + now_date + '$')
+    for (let day of historyArr) {
+        if(day !== today && dayReg.test(day)){
+            localStorage.removeItem(day);
+            console.log('remove', day);
+            let theOption = selector.querySelector(`value="${day}"`);
+            theOption && theOption.remove();
+        }
+    }
 }
 // 打开项目
 article.addEventListener('click',function (event) {
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -2,38 +2,76 @@
 
 var article = document.querySelector('article'),
     selector = document.getElementById('cache');
-//生成日期选项
-selector.innerHTML = Object.keys(localStorage).map(function (key) {
+// 根据localstorage生成日期选项
+var historyArr = Object.keys(localStorage);
+selector.innerHTML = historyArr.map(function (key) {
     return '<option value="' + key + '">' + key + '</option>';
 }).join('');
-
 selector.onchange = function () {
     article.innerHTML = localStorage.getItem(selector.selectedOptions[0].value);
 };
+
 // 获取今天的trending
-var today = new Date().getDate();
+var now = new Date();
+var now_date = now.getDate();
+var today = now.getMonth() + 1 + '-' + now_date;
 var html = localStorage.getItem(today);
 if (html) {
     article.innerHTML = html;
+    selector.value = today;
 } else {
+    // 拉取新的trending
     chrome.browserAction.setBadgeText({ text: 'load' });
     chrome.browserAction.setBadgeBackgroundColor({ color: [0, 0, 200, 255] });
+
     axios.get('https://github.com/trending').then(function (response) {
         var trendList = /<ol[\s\S]+\<\/ol>/m.exec(response.data);
         chrome.browserAction.setBadgeText({ text: 'done' });
         chrome.browserAction.setBadgeBackgroundColor({ color: [0, 0, 200, 255] });
         var html = trendList[0];
         html = html.replace(/href="\/(\S+)"/gm, 'href="https://github.com/$1"');
-        localStorage.setItem(new Date().getDate(), html);
+        localStorage.setItem(today, html);
         article.innerHTML = html;
         var option = document.createElement('option');
-        option.textContent = new Date().getDate() + '';
+        option.textContent = today;
+        option.selected = true;
         selector.appendChild(option);
     }).catch(function (e) {
         alert('数据获取不成功');
     });
+
+    // 如果有上个月当天的缓存，则清除
+    var dayReg = new RegExp('-' + now_date + '$');
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+        for (var _iterator = historyArr[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var day = _step.value;
+
+            if (day !== today && dayReg.test(day)) {
+                localStorage.removeItem(day);
+                console.log('remove', day);
+                var theOption = selector.querySelector('value="' + day + '"');
+                theOption && theOption.remove();
+            }
+        }
+    } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+    } finally {
+        try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+                _iterator.return();
+            }
+        } finally {
+            if (_didIteratorError) {
+                throw _iteratorError;
+            }
+        }
+    }
 }
-selector.value = new Date().getDate() + '';
 // 打开项目
 article.addEventListener('click', function (event) {
     if (event.target.href) {
